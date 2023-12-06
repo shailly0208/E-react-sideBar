cd6debfeb8610ef0b0eb5d60052bd3ba32c503b4
deepakdevineni <saideepakmunna@gmail.com>
2023-12-06 16:51:08 -0500

    breast cancer final update sai and krish team

--- src/screens/eir_breast_cancer_checker.jsx
+++ src/screens/eir_breast_cancer_checker.jsx
@@ -1,13 +1,14 @@
 import { useLocation } from 'react-router-dom';
-import '../styles/screens/diagonostic.css';
+import '../styles/screens/BreastCancer.css';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function BreastCancerML() {
-
+  const [showDiagnose,setShowDiagnose]=useState(false)
+  const [data,setData]=useState({})
   const location = useLocation();
   const [prediction, setPrediction] = useState('');
-  const patient_id = location.state.id;
+  const patient_id = location.state?.id;
   const BASE_URL = 'https://e-react-node-backend-22ed6864d5f3.herokuapp.com'; // Update with your node backend app URL
   //const BASE_URL = 'http://localhost:8080'; // Update with your node backend app URL
 
@@ -20,8 +21,9 @@ function BreastCancerML() {
           patient_id,
         });
         const { data } = response;
+        setData(data)
         if (data.error) {
-          alert(JSON.stringify(data.error));
+          console.log(JSON.stringify(data.error));
         } else {
           delete data.id;
           delete data.patient_id;
@@ -44,7 +46,83 @@ function BreastCancerML() {
 
   return (
     <>
-      {prediction}
+      <div className="bc-container">
+            <h2 className="title"> Breast Cancer Prediction Results</h2>
+            <div className="diagnosis-result">
+                <strong>Diagnosis:</strong> 
+            </div>
+            
+            <table className="results-tablea">
+                <thead>
+                    <tr>
+                        <th>Parameters</th>
+                        <th>Values</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                      <td>Area Mean</td>
+                      <td>{data.area_mean}</td>
+                    </tr>
+                    
+                    <tr>
+                      <td>Area Se</td>
+                      <td>{data.area_se}</td>
+                    </tr>
+                    
+                    <tr>
+                      <td>Concavity Mean</td>
+                      <td>{data.concavity_mean}</td>
+                    </tr>
+                    
+                    <tr>
+                      <td>Concavity Se</td>
+                      <td>{data.concavity_se}</td>
+                    </tr>
+                    
+                    <tr>
+                      <td>Concavity Worst</td>
+                      <td>{data.concavity_worst}</td>
+                    </tr>
+                    
+                    <tr>
+                      <td>Fractal Dimension Se</td>
+                      <td>{data.fractal_dimension_se}</td>
+                    </tr>
+                    
+                    <tr>
+                      <td>Fractal Dimension Worst</td>
+                      <td>{data.fractal_dimension_worst}</td>
+                    </tr>
+
+                    <tr>
+                      <td>Smoothness Worst</td>
+                      <td>{data.smoothness_worst}</td>
+                    </tr>
+                    
+                    <tr>
+                      <td>Symmetry Worst</td>
+                      <td>{data.symmetry_worst}</td>
+                    </tr>
+                    
+                    <tr>
+                      <td>Texture Mean</td>
+                      <td>{data.texture_mean}</td>
+                    </tr>
+                </tbody>
+            </table>
+            <br />
+            <center>
+              <button onClick={()=>setShowDiagnose(!showDiagnose)}>Diagnose</button>
+              <br /><br /><br />
+              {
+                (showDiagnose)?`${prediction}`:''
+              }
+            </center> 
+            <br />
+        </div>
+
+
     </>
   )
 }
