c2a85ef24b101c1f5489de11d51beb6f25b76cc8
ram <ramvilla997@gmail.com>
2023-12-06 15:43:38 -0500

    Diabetic

--- src/screens/DiabeticML.js
+++ src/screens/DiabeticML.js
@@ -1,35 +1,76 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useLocation } from 'react-router-dom';
 
 function DiabeticML() {
-  const [diabetesData, setDiabetesData] = useState({
+  const [diabetesData] = useState({
     "patient_id": 143,
     "pregs": 1,
     "gluc": 189,
     "bp": 60,
     "skin": 23,
-    "insulin": 846,
+    "insuli": 846,
     "bmi": 30.1,
-    "func": 0.398,
+    "fun": 0.398,
     "age": 59
   });
   const [diagnosis, setDiagnosis] = useState('');
   const [showAlert, setShowAlert] = useState(false);
   const [alertMessage, setAlertMessage] = useState('');
-  const predictDiabetes = async () => {
+  const location = useLocation();
+  const patientIdFromLocation = location.state?.id; // Check if 'id' exists in location.state
+  const [patientId, setPatientId] = useState(patientIdFromLocation);
+  const [latestRecord, setLatestRecord] = useState();
+  const [tableOfData, setTableOfData] = useState([]);
+
+  useEffect(() => {
+    // Fetch the latest record when the component mounts
+    getPatientLatestRecord();
+  }, [patientId]); // Include 'patientId' in the dependency array
+
+  const getPatientLatestRecord = async () => {
     try {
-      const response = await axios.post('https://diabetes21-11017bdfad2d.herokuapp.com/', diabetesData, {
+      const response = await axios.post('http://localhost:8080/getDiabeticsData', {
+        patientId
+      });
+      console.log("response",response);
+      const { data } = response;
+      if (data.error) {
+        alert(JSON.stringify(data.error));
+      } else if (!data || !data.data) {
+        alert("Received wrong result from the server.");
+      } else {
+        setLatestRecord(data);
+        setTableOfData(Object.values(data.data));
+      }
+    } catch (error) {
+      console.log(`Error With request on patient records: ${error.message}`);
+      alert(`An error occurred: ${error.message}`);
+    }
+  };
+
+  const predictDiabetes = async () => {
+    if (!latestRecord) {
+      alert("Please fetch the latest record first.");
+      return;
+    }
+
+    const record = latestRecord.data ?? diabetesData;
+    console.log("latest", record);
+    try {
+      const response = await axios.post('https://diabetes21-11017bdfad2d.herokuapp.com/submit', record, {
         headers: { 'Content-Type': 'application/json' },
       });
-  
+
       const result = response.data;
-  
+      console.log()
       if (response.status === 200) {
         if (result.error) {
           setAlertMessage(JSON.stringify(result.error));
         } else {
-          setDiagnosis(result);
-          setAlertMessage('Successfully received prediction');
+          setDiagnosis(result.message);
+          setAlertMessage('Successfully stored in db');
+          setShowAlert(true);
         }
       } else {
         setAlertMessage(`Error: ${result.message}`);
@@ -38,18 +79,18 @@ function DiabeticML() {
       setAlertMessage(`Error: ${error.message}`);
     } finally {
       setShowAlert(true);
-  
+
       setTimeout(() => {
         setShowAlert(false);
       }, 3000);
     }
   };
-  
 
   return (
     <>
       <div className="diabetic-container">
         <h2 className="title"> Diabetes Prediction</h2>
+        <button className="predict-button" onClick={getPatientLatestRecord}>Get Latest Record</button>
         <button className="predict-button" onClick={predictDiabetes}>Predict Diabetes</button>
         <div className="prediction-result">
           <strong>Diagnosis:</strong> {diagnosis}
